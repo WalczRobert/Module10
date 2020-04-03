2f6e255492efc73ec5cc10e3294614fc55fb98c5
rewalczak <rewalczak@gmail.com>
2020-04-03 18:55:52 -0500

    Add files via upload

--- test_student_class.py
+++ test_student_class.py
@@ -7,44 +7,44 @@ from class_definitions import student_class as sc
 
 class MyStudentClass(unittest.TestCase):
     def setUp(self):
-        self.student = sc.Student('Blankenship', 'Ryan', 'Web Development', 2.0)
+        self.student = sc.Student('Walczak', 'Robert', 'Data_Analyis', 3.6)
 
     def tearDown(self):
         del self.student
 
     def test_object_created_required_attributes(self):
-        self.assertEqual(self.student.last_name, 'Blankenship')
-        self.assertEqual(self.student.first_name, 'Ryan')
+        self.assertEqual(self.student.last_name, 'Walczak')
+        self.assertEqual(self.student.first_name, 'Robert')
 
     def test_object_created_all_attributes(self):
-        student = sc.Student('Blankenship', 'Ryan', 'Web Development', 2.0)
-        assert student.last_name == 'Blankenship'
-        assert student.first_name == 'Ryan'
-        assert student.major == 'Web Development'
-        assert student.gpa == 2.0
+        student = sc.Student('Walczak', 'Robert', 'Data_Analyis', 3.6)
+        assert student.last_name == 'Walczak'
+        assert student.first_name == 'Robert'
+        assert student.major == 'Data_Analyis'
+        assert student.gpa == 3.6
 
     def test_student_str(self):
-        self.assertEqual(str(self.student), 'Blankenship, Ryan has major Web Development with gpa: 2.0')
+        self.assertEqual(str(self.student), 'Walczak, Robert has major in Data_Analyis with gpa: 3.6')
 
     def test_object_not_created_error_last_name(self):
         with self.assertRaises(ValueError):
-            s = sc.Student('123', 'Ryan', 'Web Development', 2.0)
+            s = sc.Student('567', 'Robert', 'Data_Analyis', 3.6)
 
     def test_object_not_created_error_first_name(self):
         with self.assertRaises(ValueError):
-            s = sc.Student('Blankenship', '123', 'Web Development', 2.0)
+            s = sc.Student('Walczak', '567', 'Data_Analyis', 3.6)
 
     def test_object_not_created_error_major(self):
         with self.assertRaises(ValueError):
-            s = sc.Student('Blankenship', 'Ryan', '123', 2.0)
+            s = sc.Student('Walczak', 'Robert', '567', 3.6)
 
     def test_object_not_created_error_gpa(self):
         with self.assertRaises(ValueError):
-            s = sc.Student('Blankenship', 'Ryan', 'Web Development', 'None')
+            s = sc.Student('Walczak', 'Robert', 'Data_Analyis', 'None')
 
     def test_object_not_created_error_gpa_range_check(self):
         with self.assertRaises(ValueError):
-            s = sc.Student('Blankenship', 'Ryan', 'Web Development', 4.0)
+            s = sc.Student('Walczak', 'Robert', 'Data_Analyis', 3.6)
 
 if __name__ == '__main__':
     unittest.main()
